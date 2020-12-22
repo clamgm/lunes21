63370c9719dd0c99b2299cde1877bb3696fd6ebc
Clara <claire.060@gmail.com>
2020-12-22 12:46:19 +0100

    pagina1

--- Pagina1.php
+++ Pagina1.php
@@ -1,8 +1,33 @@
+
 <?php
 
-echo "<h1> RECETAS DULCES </h1><br/>"; 
+echo "<h1> RECETAS SALADAS </h1><br/>"; 
 
-echo "<p> Texto dela página .... </p>";
+echo "<p> <h1>Lechazo al Horno</h1></p>";
+
+echo "<p> INGREDIENTES </p>";
+
+echo "<p> 1 paletilla o pierna de cordero </p>";
+
+echo "<p> 10 cl Aove</p>";
+
+echo "<p> 10 cl vino blanco </p>";
+
+echo "<p> 2 Hojas laurel </p>";
+
+echo "<p> Tomillo, romero y perejil </p>";
+
+echo "<p> Modo de Preparación</p>";
+
+echo "<p> Precalentamos el horno a 200 ° y ponemos mientras la carne en una fuente apta para horno. </p>";
+
+echo "<p> En un tarro de cristal mezclamos el agua, el vino y el aceite con una cucharadita de sal y otra de perejil. Mezclamos agitando el tarro.</p>";
+
+echo "<p> Colocamos la fuente en la 2a guía desde abajo con la parte interior de la pierna hacia fuera y horneamos 1 hora. </p>";
+
+echo "<p> Pasado el tiempo damos la vuelta a la carne y regamos con el caldo de la fuente o del tarro si se hubiera evaporado y dejamos otra hora aproximadamente o hasta que esté dorado a nuestro gusto. Buen provecho.</p>";
+
+echo "<img src="navidad1.jpg" width="700" height="700" align="right"/> ";
 
 echo "<p> Texto dela página .... </p>";
 
@@ -14,3 +39,4 @@ echo "<p> Texto dela página .... </p>
      ";
 
 ?>
+
